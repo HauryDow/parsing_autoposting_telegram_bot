5ffb8fe96f88c2be7c5f5ac2d607564bcca16d19
HauryDow <100564592+HauryDow@users.noreply.github.com>
2022-05-18 16:13:24 +0300

    init

---
+++ parcing_bot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import telegram
+import time
+import requests
+from bs4 import BeautifulSoup
+
+TOKEN = '5317656431:AAFvdC26xtBHn-BCgJCG8q6-iWWjFl35JL8'
+bot = telegram.Bot(token=TOKEN)
+chat_id='-1001654396048'
+url='https://select.by/kursy-valyut'
+
+response=requests.get(url)
+soup=BeautifulSoup(response.text, 'lxml')#'html.parser', 'lxml'
+data=soup.find_all('div', class_='col h2')
+l=[]
+for i in data:
+    l.append(i.text)
+post=f'💵💶Лепшыя курсы валют на сёння: \n \n' \
+     f'USD 🇺🇸 : купля {l[0]}, продаж {l[1]}; \n' \
+     f'EUR 🇪🇺 : купля {l[2]}, продаж {l[3]};\n' \
+     f'RUB 🇷🇺: купля {l[3]}, продаж {l[4]}.'
+
+bot.send_message(chat_id, post)
+
+time.sleep(86400)
